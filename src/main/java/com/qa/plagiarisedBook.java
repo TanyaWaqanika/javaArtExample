56ceafb2c80afc161c2b902d87a89f7cf76e9bea
Tanya Waqanika <twaqanika31@gmail.com>
2024-09-19 14:44:08 +0100

    cleaned up code

--- src/main/java/com/qa/plagiarisedBook.java
+++ src/main/java/com/qa/plagiarisedBook.java
@@ -1,9 +1,11 @@
 package com.qa;
 
+//subclass plagiarisedBook inherits from Exception superclass
 public class plagiarisedBook extends Exception {
 
-//    Custom exception created for new books that have been plagiarised
+//    Custom exception created for new books that have been plagiarised using Override/Super constructor
     public plagiarisedBook(String message) {
         super(message);
     }
 }
+
